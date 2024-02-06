1378c4e235020087f6e06bf66db6ce1eb009d65f
mandresyyy <108409076+mandresyyy@users.noreply.github.com>
2024-02-06 11:38:05 +0300

    structure
    structure

---
+++ server.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const app = express();
+var routes= require('./routes/route');
+const mongoose = require('mongoose');
+const url = 'mongodb://127.0.0.1:27017/Coiffure';
+const cors= require('cors');
+
+mongoose.connect(url)
+const db = mongoose.connection
+db.once('open', _ => {
+  console.log('Database connected:', url)
+})
+
+db.on('error', err => {
+  console.error('connection error:', err)
+})
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3000);
