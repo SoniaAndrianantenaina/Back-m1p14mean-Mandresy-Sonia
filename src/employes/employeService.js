ef6418951e43d62515660f8846bf1918627d5c78
mandresyyy <108409076+mandresyyy@users.noreply.github.com>
2024-02-07 00:11:42 +0300

    Ajout_employe

--- src/employes/employeService.js
+++ src/employes/employeService.js
@@ -1,1 +1,21 @@
-var employeModel = require("./employeModel");
+var employe = require("./employeModel");
+const bcrypt = require("bcrypt");
+var saveemp=async(newemp)=>{
+    var emp=new employe();
+        emp.nom=newemp.nom;
+        emp.prenom=newemp.prenom;
+        emp.email=newemp.email;
+        emp.mdp=await bcrypt.hash(newemp.mdp,10);
+        emp.h_debut=newemp.h_debut;
+        emp.h_fin=newemp.h_fin;
+    try{
+        await emp.save();
+        return true;
+    }
+    catch(error){
+        console.error('Erreur:',error);
+        return false;
+    }
+}
+
+module.exports={saveemp};
