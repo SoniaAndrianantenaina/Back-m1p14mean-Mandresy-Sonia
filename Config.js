979c9963bce5c2a27db1c9da0868ff986e46fb7d
Sonia Andrianantenaina <andrianantenainasonia@gmail.com>
2024-02-29 14:42:13 +0100

    changement base

--- Config.js
+++ Config.js
@@ -1,10 +1,10 @@
-const db_name="Coiffure";
-const use_https= false; // https ou http utile pour la config de la session
-const mail_config={
-    "user":"andriantsiorymandresy0@gmail.com",
-    "mdp":"fzxx kgxs yigu mucp"
+const db_name = "coiffure";
+const use_https = false; // https ou http utile pour la config de la session
+const mail_config = {
+  user: "andriantsiorymandresy0@gmail.com",
+  mdp: "fzxx kgxs yigu mucp",
 };
 
-const heure_envoie_notif="44 12 * * *"// min h j mois annee
+const heure_envoie_notif = "44 12 * * *"; // min h j mois annee
 
-module.exports={db_name, use_https,mail_config,heure_envoie_notif}
+module.exports = { db_name, use_https, mail_config, heure_envoie_notif };
--- server.js
+++ server.js
@@ -1,34 +1,37 @@
-const express=require('express');
-const session = require('express-session');
-const bodyParser = require('body-parser');
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
 const app = express();
-const cors= require('cors');
-var routes= require('./routes/route');
-const mongoose = require('mongoose');
-const config=require('./Config.js');
-const url = 'mongodb://127.0.0.1:27017/'+config.db_name;
-const crontTasks = require('./src/envoie_notif.js');
+const cors = require("cors");
+var routes = require("./routes/route");
+const mongoose = require("mongoose");
+const config = require("./Config.js");
+const url =
+  "mongodb+srv://andrianantenainasonia:sonia0688@cluster0.jj3sjrp.mongodb.net/" +
+  config.db_name;
+const crontTasks = require("./src/envoie_notif.js");
 
-app.use(session({
-  secret: 'MaCléSecrète123!',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: config.use_https } // Remplacez false par true si vous utilisez HTTPS
-}));
+app.use(
+  session({
+    secret: "MaCléSecrète123!",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: config.use_https }, // Remplacez false par true si vous utilisez HTTPS
+  })
+);
 
+mongoose.connect(url);
+const db = mongoose.connection;
+db.once("open", (_) => {
+  console.log("Database connected:", url);
+});
 
-mongoose.connect(url)
-const db = mongoose.connection
-db.once('open', _ => {
-  console.log('Database connected:', url)
-})
+db.on("error", (err) => {
+  console.error("connection error:", err);
+});
 
-db.on('error', err => {
-  console.error('connection error:', err)
-})
-
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(cors());
 app.use(express.json());
